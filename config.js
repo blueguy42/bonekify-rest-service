7cc9e67b22f0e4a4ebaf79f59e8773cfc139fbbb
Ahmad Alfani Handoyo <ahmadalfanihandoyo@hotmail.com>
2022-11-30 20:50:48 +0700

    fix: db connection in docker

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
     db: {
       /* don't expose password or any sensitive info, done only for demo */
-      host: "localhost",
-      port: 1502,
+      host: "db-rest",
+      port: 3306,
       user: "root",
       password: "root",
       database: "rest_database",
