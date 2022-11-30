1081d4fc5dfa998047381ce31248f3cfb4da33e9
Ahmad Alfani Handoyo <ahmadalfanihandoyo@hotmail.com>
2022-11-30 20:58:06 +0700

    refactor: dump penyanyi

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ const config = {
       /* don't expose password or any sensitive info, done only for demo */
       host: "db-rest",
       port: 3306,
+      // host: "localhost",
+      // port: 1502,
       user: "root",
       password: "root",
       database: "rest_database",
--- dump/dump.sql
+++ dump/dump.sql
@@ -28,6 +28,9 @@ CREATE TABLE IF NOT EXISTS `Song` (
 
 INSERT INTO `User` (`email`, `password`, `username`, `name`, `isAdmin`) VALUES
 ('admin@gmail.com', '$2b$10$pnqu3Rh/ozFTIebO3ze3XOs1uX849gHmnycdB36V7y.hGTcK7huKe', 'admin', 'Admin', 1),
-('penyanyi@gmail.com', '$2b$10$RjAtP0QExrkp8/c2qtgbV.3Yz6a9yVzfbjXmi3uu8WdwExN/v4nXW', 'penyanyi', 'Penyanyi', 0);
+('penyanyi@gmail.com', '$2b$10$RjAtP0QExrkp8/c2qtgbV.3Yz6a9yVzfbjXmi3uu8WdwExN/v4nXW', 'penyanyi', 'Penyanyi', 0),
+('blackpink@gmail.com', '$2b$10$j2I/ZDqT42eKLjWZa9EGJ.H7aHxNenScH2kZtM1fIw9vjjxbI3dy2', 'blackpink', 'BLACKPINK', 0),
+('katyperry@gmail.com', '$2b$10$hK2CEDkiLBdSpoS4eoOAh.FxsWKt6cMuHZPSlACN2VgDHUG.lph0G', 'katyperry', 'Katy Perry', 0),
+('coldplay@gmail.com', '$2b$10$9udMfUEZf4nSiEhga3hRB.aWgGISFjvkVOAIdtffl.oYOFCSvhZ.6', 'coldplay', 'Coldplay', 0),
 
 COMMIT;
